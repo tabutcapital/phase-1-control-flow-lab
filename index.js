fa5738f49811d1cd8bbf78f152ceb5124451b488
tabutcapital <iantabut3@gmail.com>
2024-10-22 16:18:17 +0300

    lab done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return "This one is on me!"
+  } else if (feet >= 400 && feet <= 2000){
+    return "That will be twenty bucks."
+  } else if (feet > 2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks."
+  } else (feet > 2500)
+    return "No can do."
+  
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'Pittsburgh' ? "No go." : "Ok, sounds good."
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+      default:
+      return 'Bye.'
+      
+    
+  }
+  
 }
